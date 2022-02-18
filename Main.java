ceb605858c15b4d4df8743ad2b33a6b3f53f93fa
Alex-programist <92448729+Alex-programist@users.noreply.github.com>
2022-02-18 20:33:36 +0200

    1 лр 2 завдання
    1 лр 2 завдання

--- Main.java
+++ Main.java
@@ -3,24 +3,28 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        float y;
-        System.out.println("Введіть значення х та z");
-        int x = in.nextInt(),z = in.nextInt();
-        if(x==5)
-            System.out.println("Значення функції y="+ (y=3+x*z));
-        if(x==-5)
-            System.out.println("Значення функції y="+ (y=(x*z)/(7-z)));
-        if(x==0)
-            System.out.println("Значення функції y="+ (y=x));
-        /*
-           i  System.f(x==5)
-          System.out.println("Значення функції y="+ (y=3+x*z));
-           else if(x==-5)
-            System.out.println("Значення функції y="+ (y=(x*z)/(7-z)));
-           else if(x==0)
-            System.out.println("Значення функції y="+ (y=x));
-         */
+    static boolean function(int circle_x, int circle_y,int rad, int x, int y)
+    {
+        if ((x - circle_x) * (x - circle_x) +
+                (y - circle_y) * (y - circle_y) <= rad * rad)
+            return true;
+        else
+            return false;
     }
-}
+        public static void main(String arg[])
+        {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введіть кординати точки A");
+            int x = in.nextInt(), y = in.nextInt();;
+            System.out.println("Введіть кординати центра кола та радіус");
+            int circle_x = in.nextInt(), circle_y = in.nextInt(), rad = in.nextInt();
+            if (function(circle_x, circle_y, rad, x, y))
+                System.out.print("Точка находиться всередині кола");
+            else
+                System.out.print("Точка находиться зовні кола");
+        }
+    }
+
+
+
+
