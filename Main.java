6fd99b7d4c1ff61087c1fe749890709fb254ff9f
Alex-programist <92448729+Alex-programist@users.noreply.github.com>
2022-03-30 21:02:40 +0300

    лр4

--- Main.java
+++ Main.java
@@ -1,38 +1,29 @@
 package com.company;
+
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         String n ;
         Scanner in = new Scanner(System.in);
         do {
-            System.out.println("1-Обчислення функції");
-            System.out.println("2-Знаходження квадрату суми непарних чисел в діапазоні [1;10]");
-            System.out.println("3-Знаходження суми за формулою");
-            System.out.println("4-Вивести значення функції у табличному вигляді");
-            System.out.println("5-Закінчити роботу");
+            System.out.println("1 - Завдання 1");
+            System.out.println("2 - Завдання 2");
+            System.out.println("3 - Завершити роботу");
             n=in.next();
             switch (n) {
                 case "1":
-                    System.out.println("Завдання 1");
-                    Task1.Start();
+                    System.out.println("Завдання 1 - Поміняти перший і останій стовпці матриці.");
+                    Task1.mat();
                     break;
                 case "2":
-                    System.out.println("Завдання 2");
-                    Task2.Start();
+                    System.out.println("Завдання 2 - Ввести квадратну матрицю цілих чисел. Визначити, чи ця матриця є магiчним квадратом, для якого суми елементiв кожного рядка i стовпчика є однаковими..");
+                    Task2.squere();
                     break;
                 case "3":
-                    System.out.println("Завдання 3");
-                    Task3.Start();
-                    break;
-                case "4":
-                    System.out.println("Завдання 4");
-                    Task4.Start();
-                    break;
-                case "5":
                     return;
             }
-
-        } while (n!="5");
+        } while (n!="3");
     }
 }
